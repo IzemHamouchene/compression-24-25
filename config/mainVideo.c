0ad99e5c8ff34e8ea33f1a5f17cf16c6268b8e1d
IzemHamouchene <77283279+IzemHamouchene@users.noreply.github.com>
2025-01-26 21:44:05 +0100

    Update mainVideo.c

--- config/mainVideo.c
+++ config/mainVideo.c
@@ -1,21 +1,21 @@
 // config.c
 #include <stdio.h>
+#include <string.h>
 #include <assert.h>
 #include <stdlib.h>
 
-#include "video.h"
+#include "compression.h"
 #include "assert_with_message.h"
 
-int testMethod(int input1, int input2, int durationMovie, int durationCredit, int fps, char* unit, int output){
+int testMethod(char* inputText, int output){
     char message[100];
-    int result = video(input1, input2, durationMovie, durationCredit, fps, unit);
-    sprintf(message, "Inputs (%d, %d). Expected output should be %d. Actuel output = %d", input1, input2, output, result);
-    assert_with_message(((result >= output - 0.2) && (result <= output + 0.2)), message);
+    char* result = compression(inputText);
+    sprintf(message, "Inputs (%s). Expected output should be (%s). Actuel output = (%s)", inputText, output, result);
+    assert_with_message(strcmp( result, output ) != 0, message);
 }
 
 int main() {
-    testMethod(1024, 1024, 30, 10, 20, "go", 1.953125);
-
-    testMethod(500, 300, 60, 10, 20, "mo", 543.594360);
+    testMethod("aaaaabbbccdeeee", "#5abbbccd#4e");
+    testMethod("aaaaabbbccddeee", "#5abbbccddeee");
     return 0;
 }
